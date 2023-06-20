68c2e2a3aebb12ca335f102e5a6d5528dbaa952d
Yuriy <artentrery@gmail.com>
2023-06-20 22:59:09 +0300

    Part 2 - Init and work with double. Add iomanip.

--- hw3/hw3.cpp
+++ hw3/hw3.cpp
@@ -1,8 +1,9 @@
 ﻿#include <iostream>
+#include <iomanip>
 
 int main()
 {
-    // 1 - Change value of 2 input numbers 
+    // Part 1 - Change value of 2 input numbers
     int num1;
     int num2;
     int temp;
@@ -16,8 +17,20 @@ int main()
     num1 = num2;
     num2 = temp;
     
-    std::cout << "integer 1: " << num1 << "\n";
-    std::cout << "integer 2: " << num2 << "\n";
+    std::cout << "integer 1: " << num1 << std::endl;
+    std::cout << "integer 2: " << num2 << std::endl;
+
+    // Part 2 - Init and work with double
+    /*
+        Створити та ініціалізувати змінну типу double, вивести значення змінної в консолі; вивести значення змінної приведене до типу int.
+        (без символів після коми); Вивести число в рядок шириною 20 символів, вирівняти по правому краю, пусте місце заповнити символом ‘$’(дивись I / O manipulators)
+    */
+
+    double value = 23.345383487;
+
+    std::cout << value << std::endl;
+    std::cout << (int) value << std::endl;
+    std::cout << std::setw(20) << std::right << std::setfill('$') << value << std::endl;
 
     return 0;
 }
