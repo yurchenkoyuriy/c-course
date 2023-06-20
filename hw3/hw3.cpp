0571d29a7f7145d5d73c2a5d865e184a9f2a5965
Yuriy <artentrery@gmail.com>
2023-06-20 22:47:52 +0300

    Part 1 - Change value of 2 input numbers

---
+++ hw3/hw3.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+int main()
+{
+    // 1 - Change value of 2 input numbers 
+    int num1;
+    int num2;
+    int temp;
+
+    std::cout << "Enter integer 1: ";
+    std::cin >> num1;    
+    std::cout << "Enter integer 2: ";
+    std::cin >> num2;
+    
+    temp = num1;
+    num1 = num2;
+    num2 = temp;
+    
+    std::cout << "integer 1: " << num1 << "\n";
+    std::cout << "integer 2: " << num2 << "\n";
+
+    return 0;
+}
