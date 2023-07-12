728003e314d310f3022044ff4532643004fa2c33
Yuriy <artentrery@gmail.com>
2023-07-12 18:17:42 +0300

    Home work 8

---
+++ hw8/MathFunctions.h
@@ -0,0 +1,7 @@
+void counting(int count);
+int factorial(int count);
+int factorialRecursion(int count);
+void printNaturalNumbersAscending(int count);
+void printNaturalNumbersDescending(int count);
+void printNaturalNumbersAscendingRecursion(int count);
+void printNaturalNumbersDescendingRecursion(int count);
