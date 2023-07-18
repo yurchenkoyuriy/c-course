01ebd907086d678c4f8ae916a79a8a8524ef878e
Yuriy <artentrery@gmail.com>
2023-07-18 17:42:01 +0300

    home work 10

---
+++ hw10/HomeFunctions.h
@@ -0,0 +1,4 @@
+void swap(int& a, int& b); 
+void swap(int* a, int* b);
+bool calculateSum(const double* arr, int arrSize, double& sum);
+bool find(const int* arr, int size, int elem);
---
+++ hw10/hw10.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+#include "HomeFunctions.h"
+
+int main()
+{
+    // Part 1
+    int a{1};
+    int b{2};
+        
+    swap(a, b);
+
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
+
+    int* c = &a;
+    int* d = &b;
+
+    swap(c, d);
+
+    std::cout << *c << std::endl;
+    std::cout << *d << std::endl;
+
+    // Part 2
+
+    const short int sizeArr = 5;
+    double arr[sizeArr];
+    double sum{};
+
+    std::cout << "Enter " << sizeArr << " numbers of array: ";
+
+    for (int i = 0; i < sizeArr; i++)
+    {
+        std::cin >> arr[i];
+    }
+    
+    if (calculateSum(arr, sizeArr, sum))
+    {
+        std::cout << "Suma of elements is: " << sum << std::endl;
+    }
+    
+    // Part 3
+
+    const short int size = 5;
+    int arr_find[size];
+    int elem{};
+
+    std::cout << "Enter element we want to found: ";
+    std::cin >> elem;
+
+    std::cout << "Enter " << size << " numbers of array: ";
+
+    for (int i = 0; i < size; i++)
+    {
+        std::cin >> arr_find[i];
+    }
+
+    if (find(arr_find, size, elem))
+    {
+        std::cout << "Element " << elem << " was found!";
+    } 
+    else 
+    {
+        std::cout << "Element " << elem << " not found!";
+    }
+}
